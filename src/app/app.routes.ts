902899bc640508303e3457539ae6edb2fff82c41
erick ayara <ayaraerick@gmail.com>
2024-01-05 22:33:29 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 // import path from 'path';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full'},
 ];
 
